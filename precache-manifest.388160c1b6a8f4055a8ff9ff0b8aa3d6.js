8da633f1035a7dceccfd13743aee4c499d34be30
habimendoza <habimendoza@hotmail.com>
2020-01-20 17:45:55 -0500

    Updates

---
+++ precache-manifest.388160c1b6a8f4055a8ff9ff0b8aa3d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6470ca303ef01c3ea977bf4e90dfef9d",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "8bf74dc00ccd4cb941cb",
+    "url": "/newsapp/static/css/main.8fc82219.chunk.css"
+  },
+  {
+    "revision": "773061a4336f4d03d28f",
+    "url": "/newsapp/static/js/2.bb1a174d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newsapp/static/js/2.bb1a174d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bf74dc00ccd4cb941cb",
+    "url": "/newsapp/static/js/main.c3b195c4.chunk.js"
+  },
+  {
+    "revision": "4f51200ce2e4dc303dfc",
+    "url": "/newsapp/static/js/runtime-main.da1f3a28.js"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/newsapp/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/newsapp/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/newsapp/static/media/Montserrat-Medium.c8b6e083.ttf"
+  }
+]);
